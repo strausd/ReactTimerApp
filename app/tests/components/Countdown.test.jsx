f00ecc1d3795a5d81a95884c81ab6118e0f15ccf
Daniel Straus <strausd@me.com>
2017-04-19 14:52:53 -0500

    Finalize tests & add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -23,6 +23,7 @@ describe('Countdown', () => {
                 done();
             }, 1001);
         });
+
         it('should not set count to a negative number', (done) => {
             var countdown = TestUtils.renderIntoDocument(<Countdown/>);
             countdown.handleSetCountdown(1);
@@ -31,6 +32,30 @@ describe('Countdown', () => {
                 expect(countdown.state.count).toBe(0);
                 done();
             }, 3001);
+        });
+
+        it('should pause countdown on paused status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe('paused');
+                done();
+            }, 1001);
+        });
+
+        it('should reset count on stopped status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe('stopped');
+                done();
+            }, 1001);
         });
     });
 });
