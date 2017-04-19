1fa80713330836843cb3cd575aaac7247f010e9c
Daniel Straus <strausd@me.com>
2017-04-19 11:36:25 -0500

    Create and test CountdownForm component

---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var CountdownForm = React.createClass({
+    onSubmit: function (e) {
+        e.preventDefault();
+        var strSeconds = this.refs.seconds.value;
+        if(strSeconds.match(/^[0-9]*$/)) {
+            this.refs.seconds.value = '';
+            this.props.onSetCountdown(parseInt(strSeconds, 10));
+        }
+    },
+    render: function () {
+        return (
+            <div className="ui grid centered">
+                <div className="three wide column">
+                    <form ref="form" onSubmit={this.onSubmit} className="countdown-form">
+                        <div className="ui fluid input form-element">
+                            <input type="text" placeholder="Enter time in seconds" ref="seconds"/>
+                        </div>
+                        <div className="form-element">
+                            <button className="ui fluid button">Start</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = CountdownForm;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,9 @@ var Navigation = require('Navigation');
 var Main = (props) => {
     return (
         <div>
+            <Navigation/>
             <div>
                 <div>
-                    <Navigation/>
-                    <p>Main.jsx Rendered!</p>
                     {props.children}
                 </div>
             </div>
